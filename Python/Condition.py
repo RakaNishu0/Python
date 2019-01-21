4a5c94c192fee1314aa2f5017357d5eb3f335bca
RakaNishu0 <getouthere0@gmail.com>
2019-01-22 01:08:33 +0900

    1st version

---
+++ Python/Condition.py
@@ -0,0 +1,36 @@
+if False:
+    print("code1")
+    print("code2")
+    print("code3")
+print("------1st session Ended")
+
+input = 11
+real = 11
+
+if real == input:
+    print("Hello~")
+
+print("------2nd session Ended")
+
+input = 11
+real = 11
+
+if real == input:
+    print("Hello~")
+else:
+    print("Who Are YOU?")
+print("------3rd session Ended")
+
+input = 11
+real_raka = 11
+real_nishu = "ab"
+
+if real_raka == input:
+    print("Hello~ raka")
+elif real_nishu== input:
+    print("Hello~ nishu")
+else:
+    print("Who Are YOU?")
+print("------4th session Ended")
+
+print("test for Git")
