b20fd3b5d3accd2ebe429dcfb2fe69ea3d39d0a6
RakaNishu0 <getouthere0@gmail.com>
2019-01-26 15:37:18 +0900

    conta_loop

---
+++ Python/Conta_Loop/3.py
@@ -0,0 +1,15 @@
+# i = [0,1,2,3,4,5]
+#
+# for item in i:
+#     print(item)
+
+
+# for item in range(5):
+#     print(item)
+
+
+for item in [5,6,7,8,9,10]:
+    print(item)
+
+for item in range(5,11):
+    print(item)
---
+++ Python/Conta_Loop/4.py
@@ -0,0 +1,20 @@
+inp_value = input("Your ID..? : ")      # python의 input()은 문자열로만 반환한다.
+
+members = ['raka','nishu','luke','skywalker']
+
+for id in members:
+    if id == inp_value:
+        print("Hello "+inp_value)
+        import sys
+        sys.exit()
+print("Who Are You?")
+
+# real_raka = 11
+# real_nishu = "nishu"
+#
+# if real_raka == inp_value:              # 문자열 vs 숫자의 비교이므로 False
+#     print("Hello raka")
+# elif real_nishu.upper() == inp_value.upper():
+#     print("Hello nishu")
+# else:
+#     print("Get Out!")
