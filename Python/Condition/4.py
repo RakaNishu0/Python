9520840bcc71cf073e86f88289e2f27438d99f95
RakaNishu0 <getouthere0@gmail.com>
2020-09-27 22:12:56 +0900

    if - 2

--- Python/Condition/4.py
+++ Python/Condition/4.py
@@ -5,7 +5,7 @@ real_nishu = 22
 
 if real_raka == inp_value:
     print("Hello raka")
-elif real_nishu == inp_value:
-    print("Hello nishu")
+elif real_nishu == inp_value:       # elif = elseif / if의 조건이 False일 때에 다시 한번 실행될 조건을 지정 / elif는 복수설정 가능
+    print("Hello nishu")            # elif 의 조건이 True일 때 실행되는 부분
 else:
-    print("Get Out!")
+    print("Get Out!")               # else 는 elif의 조건마저도 False일 때 출력할 결과를 지정하게 된다
