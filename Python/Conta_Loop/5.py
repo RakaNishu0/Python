aa1304312ff3de4ba02e8297c7127971cc60fcbf
RakaNishu0 <getouthere0@gmail.com>
2020-09-27 23:12:35 +0900

    loop 1

---
+++ Python/Conta_Loop/5.py
@@ -0,0 +1,16 @@
+inp_v = input("Year? :")
+inp_v = int(inp_v)
+
+#if inp_v ==
+#    print("Error. Only int()")
+#else:
+#    print("Calculating...")
+
+#while inp_v >= 4:
+#    inp_v = inp_v/4
+#    print(inp_v)
+
+if inp_v % 4 == 0:
+    print("Leaf Year")
+else:
+    print("Normal Year")
---
+++ Python/Loop/3.py
@@ -0,0 +1,8 @@
+i = 0
+k = 1
+
+while k <= 100:
+    if k % 3 == 0:
+        i = i + k
+    k = k + 1
+print(i)
