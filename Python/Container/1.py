41e8ac3e1bb28b2d30a10a2686eef036a4ae2265
mhyoo <mhyoo@eduhansol.co.kr>
2020-09-28 16:59:07 +0900

    1

--- Python/Container/1.py
+++ Python/Container/1.py
@@ -12,9 +12,9 @@ print(names)
 # index는 0부터 시작한다.
 print(names[1])
 
-RakaNishu = ['Man', 'Old', 22, False]
-RakaNishu[2] = 36
+RakaNishu = ['Man', 'Old', 22, False]           # List의 데이터는 문자, 숫자 등 여러 종류가 가능하다
+RakaNishu[2] = 36                               # index 2의 데이터를 36으로 치환한다
 
-RakaNishu.append('No Manner')
+RakaNishu.append('No Manner')                   
 print(len(RakaNishu))
 print(RakaNishu)
