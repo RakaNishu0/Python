b03d2ebb42d106652086a8ad4c078f828ae2ff30
RakaNishu0 <getouthere0@gmail.com>
2020-09-29 19:44:58 +0900

    0929

--- Python/Loop/1.py
+++ Python/Loop/1.py
@@ -9,7 +9,8 @@
 # print('Hello World 72')
 # print('Hello World 81')
 
-i=0
-while i < 10:
-    print("print('Hello World "+str(i*9)+"')")
-    i=i+1
+i = 0
+while i < 10:           # while 의 뒤에는 Boolean 이 따라와야 한다 (True or False)
+    print("print('Hello World "+str(i*9)+"')")      # i*9 는 숫자데이터인데, 문자열 사이에 합칠 수 없으므로 이를 문자열(str)화 하여야 한다
+    i = i+1
+print("while Ended")
--- Python/Loop/2.py
+++ Python/Loop/2.py
@@ -19,4 +19,4 @@ i = 0
 while i <10:
     if i != 4:
         print(i)
-    i=i+1
+    i = i+1
